62bdc030e7d1adc27e7d1593df05c28f1b8700d1
PuerNya <safarier@outlook.com>
2024-02-05 16:40:03 +0800

    Add zh-cn translate

--- app/src/main/java/io/nekohasekai/sfa/constant/EnabledType.kt
+++ app/src/main/java/io/nekohasekai/sfa/constant/EnabledType.kt
@@ -1,11 +1,16 @@
 package io.nekohasekai.sfa.constant
 
+import io.nekohasekai.sfa.R
+
 enum class EnabledType(val boolValue: Boolean) {
     Enabled(true), Disabled(false);
 
     companion object {
         fun from(value: Boolean): EnabledType {
             return if (value) Enabled else Disabled
+        }
+        fun valueOf(value: String): EnabledType {
+            return if (value == R.string.enabled) Enabled else Disabled 
         }
     }
 }
