c05170482f4cb57614dbe6ac948de498b537f88a
reF1nd <ref1nd@thunder-cloud.cc>
2026-03-24 21:44:27 +0800

    Remove F-Droid features

--- app/src/main/java/io/xireiki/sfa/vendor/VendorInterface.kt
+++ app/src/main/java/io/xireiki/sfa/vendor/VendorInterface.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import androidx.camera.core.ImageAnalysis
 import io.xireiki.sfa.compose.screen.qrscan.QRCodeCropArea
 import io.xireiki.sfa.update.UpdateInfo
-import io.xireiki.sfa.update.UpdateSource
 
 interface VendorInterface {
     fun checkUpdate(activity: Activity, byUser: Boolean)
@@ -18,8 +17,6 @@ interface VendorInterface {
     fun isPerAppProxyAvailable(): Boolean = true
 
     val hasCustomUpdate: Boolean get() = false
-
-    val updateSources: List<UpdateSource> get() = listOf(UpdateSource.GITHUB)
 
     fun checkUpdateAsync(): UpdateInfo? = null
 
