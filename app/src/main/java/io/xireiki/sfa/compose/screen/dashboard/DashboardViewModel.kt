f32940c8e071be45b1990acddf960c0afc6d5344
reF1nd <ref1nd@thunder-cloud.cc>
2026-06-17 01:02:12 +0800

    Reduce background work and plug runtime leaks

--- app/src/main/java/io/xireiki/sfa/compose/screen/dashboard/DashboardViewModel.kt
+++ app/src/main/java/io/xireiki/sfa/compose/screen/dashboard/DashboardViewModel.kt
@@ -119,6 +119,7 @@ class DashboardViewModel :
     CommandClient.Handler {
     private val _serviceStatus = MutableStateFlow(Status.Stopped)
     val serviceStatus: StateFlow<Status> = _serviceStatus.asStateFlow()
+    private var pendingToggle = false
 
     internal val commandClient =
         CommandClient(
@@ -239,9 +240,17 @@ class DashboardViewModel :
 
     fun toggleService() {
         when (currentState.serviceStatus) {
-            Status.Starting, Status.Started -> stopService()
-            Status.Stopped -> sendGlobalEvent(UiEvent.RequestStartService)
-            else -> { /* Ignore while transitioning */ }
+            Status.Started -> {
+                pendingToggle = false
+                stopService()
+            }
+            Status.Stopped -> {
+                pendingToggle = false
+                sendGlobalEvent(UiEvent.RequestStartService)
+            }
+            Status.Starting, Status.Stopping -> {
+                pendingToggle = !pendingToggle
+            }
         }
     }
 
@@ -455,6 +464,19 @@ class DashboardViewModel :
                 )
             }
             handleServiceStatusChange(status)
+            if (pendingToggle) {
+                when (status) {
+                    Status.Started -> {
+                        pendingToggle = false
+                        stopService()
+                    }
+                    Status.Stopped -> {
+                        pendingToggle = false
+                        sendGlobalEvent(UiEvent.RequestStartService)
+                    }
+                    else -> {}
+                }
+            }
         }
     }
 
